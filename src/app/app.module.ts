aa50f64ff53dd1baf80c7447fd0702221ccbc955
SHERVIL GUPTA <shervilgupta@Shervil.local>
2020-01-17 13:19:01 +0530

    item component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { ResultComponent } from './result/result.component';
 import {ResultService} from './result.service';
+import { ItemComponent } from './item/item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchBarComponent,
-    ResultComponent
+    ResultComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule
