f4f717e2bbbd128fbb7f6cb94bf59fafe1c950a4
SHERVIL GUPTA <shervilgupta@Shervil.local>
2020-01-17 15:37:33 +0530

    chirag bhai sambhal

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -9,14 +9,19 @@ export class ItemComponent implements OnInit {
   public itemName: string;
   public itemPrice: number;
   public itemQuantity: number;
-  public itemImage: string;
+  public itemImage: String;
   constructor() {
     this.itemName = 'NO NAME';
     this.itemPrice = 0;
     this.itemQuantity = 0;
-    this.itemImage = 'default.png';
+    this.itemImage = 'default';
   }
-  ngOnInit() {
+  change() {
+   const x = document.getElementById('t1');
+   this.itemQuantity = parseInt(x.innerText.toString());
+   this.itemPrice = (this.itemQuantity * this.itemPrice);
+   const y = document.getElementById('t2');
+   alert(this.itemPrice);
   }
-
+  ngOnInit() { }
 }
